fa3a3b80113ca49ec24e8088ac42e5dc2f6b1a90
Jaikumar <jaikumar.vaswani@hotmail.com>
2024-04-15 03:43:38 +0530

    Renames the repository model.

---
+++ repository/models/tiger_details_with_sightings.go
@@ -0,0 +1,13 @@
+package models
+
+import "time"
+
+type TigerDetailWithSightings struct {
+	TigerId      int64     `db:"tiger_id" json:"tigerId"`
+	TigerName    string    `db:"name" json:"tigerName"`
+	DateOfBirth  time.Time `db:"dob" json:"dob"`
+	LastSeen     time.Time `db:"last_seen" json:"lastSeen"`
+	LastPosition Location  `db:"last_position" json:"lastPosition"`
+	ReportedBy   int64     `db:"user_id" json:"-"`
+	Image        []byte    `db:"image" json:"-"`
+}
